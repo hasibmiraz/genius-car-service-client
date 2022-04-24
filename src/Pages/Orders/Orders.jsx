95d0655f174b7cf435b842b06b61599469634784
hasibmiraz <hasibmiraz09@gmail.com>
2022-04-25 01:24:16 +0600

    Added JWT Authentication

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import axios from 'axios';
+import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import PageTitle from '../Shared/PageTitle/PageTitle';
 
 const Orders = () => {
+  const [user] = useAuthState(auth);
+  const [orders, setOrders] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const { email } = user;
+    const getOrders = async () => {
+      const url = `http://localhost:5000/order?email=${email}`;
+      try {
+        const { data } = await axios.get(url, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+          },
+        });
+        setOrders(data);
+      } catch (err) {
+        console.log(err.message);
+        if (err.response.status === 401 || err.response.status === 403) {
+          signOut(auth);
+          navigate('/login');
+        }
+      }
+    };
+    getOrders();
+  }, [user, navigate]);
   return (
     <div>
-      <h2>Your orders</h2>
+      <PageTitle title="Orders"></PageTitle>
+      <h2>Your orders: {orders.length}</h2>
     </div>
   );
 };
